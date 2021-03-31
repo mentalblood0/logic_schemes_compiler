c6be51ba0a9937938f64334b706c7272ca2b60a4
Нечепоренко Степан Юрьевич <necheporenko_s_iu@gz.local>
2021-03-31 13:29:15 +0300

    fixed bug

--- example.c
+++ example.c
@@ -6,16 +6,20 @@
 #define AND(x, y) ((x) && (y))
 #define OUTPUT(x) (x)
 
-#define __xor__NOT_1(__xor__INPUT_1, __xor__INPUT_2) NOT(__xor__INPUT_1(__xor__INPUT_1, __xor__INPUT_2))
-#define __xor__NOT_2(__xor__INPUT_1, __xor__INPUT_2) NOT(__xor__INPUT_2(__xor__INPUT_1, __xor__INPUT_2))
-#define __xor__OR_2(__xor__INPUT_1, __xor__INPUT_2) OR(__xor__INPUT_1(__xor__INPUT_1, __xor__INPUT_2), __xor__INPUT_2(__xor__INPUT_1, __xor__INPUT_2))
+#define __xor__NOT_1(__xor__INPUT_1, __xor__INPUT_2) NOT(__xor__INPUT_1)
+#define __xor__NOT_2(__xor__INPUT_1, __xor__INPUT_2) NOT(__xor__INPUT_2)
+#define __xor__OR_2(__xor__INPUT_1, __xor__INPUT_2) OR(__xor__INPUT_1, __xor__INPUT_2)
 #define __xor__OR_1(__xor__INPUT_1, __xor__INPUT_2) OR(__xor__NOT_1(__xor__INPUT_1, __xor__INPUT_2), __xor__NOT_2(__xor__INPUT_1, __xor__INPUT_2))
 #define __xor__AND_1(__xor__INPUT_1, __xor__INPUT_2) AND(__xor__OR_1(__xor__INPUT_1, __xor__INPUT_2), __xor__OR_2(__xor__INPUT_1, __xor__INPUT_2))
 #define __xor__OUTPUT_1(__xor__INPUT_1, __xor__INPUT_2) OUTPUT(__xor__AND_1(__xor__INPUT_1, __xor__INPUT_2))
 #define xor(__xor__INPUT_1, __xor__INPUT_2) __xor__OUTPUT_1(__xor__INPUT_1, __xor__INPUT_2)
 
-#define __xorxor__xor_1(__xorxor__INPUT_1, __xorxor__INPUT_2) xor(__xorxor__INPUT_1(__xorxor__INPUT_1, __xorxor__INPUT_2), __xorxor__INPUT_2(__xorxor__INPUT_1, __xorxor__INPUT_2))
-#define __xorxor__xor_2(__xorxor__INPUT_1, __xorxor__INPUT_2) xor(__xorxor__INPUT_1(__xorxor__INPUT_1, __xorxor__INPUT_2), __xorxor__INPUT_2(__xorxor__INPUT_1, __xorxor__INPUT_2))
+#define __xorxor__xor_1(__xorxor__INPUT_1, __xorxor__INPUT_2) xor(__xorxor__INPUT_1, __xorxor__INPUT_2)
+#define __xorxor__xor_2(__xorxor__INPUT_1, __xorxor__INPUT_2) xor(__xorxor__INPUT_1, __xorxor__INPUT_2)
 #define __xorxor__xor_3(__xorxor__INPUT_1, __xorxor__INPUT_2) xor(__xorxor__xor_1(__xorxor__INPUT_1, __xorxor__INPUT_2), __xorxor__xor_2(__xorxor__INPUT_1, __xorxor__INPUT_2))
 #define __xorxor__OUTPUT_1(__xorxor__INPUT_1, __xorxor__INPUT_2) OUTPUT(__xorxor__xor_3(__xorxor__INPUT_1, __xorxor__INPUT_2))
 #define xorxor(__xorxor__INPUT_1, __xorxor__INPUT_2) __xorxor__OUTPUT_1(__xorxor__INPUT_1, __xorxor__INPUT_2)
+
+int main(void) {
+	printf("%d", xorxor(0, 1));
+}
--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 import json
 
+def defineFunction(description):
+	result = ''
+
+	unique_elements = {}
+	elements_inputs = {}
+	for w in description['wires']:
+		unique_elements[w['from']] = None
+		unique_elements[w['to']] = None
+		element = w['to'].split('[')[0]
+		if not (element in elements_inputs):
+			elements_inputs[element] = {}
+		input_index = w['to'][:-1].split('[')[-1]
+		try:
+			elements_inputs[element][int(input_index)] = w['from']
+		except ValueError:
+			elements_inputs[element][0] = w['from']
+
+	inputs_number = len(list(filter(lambda e: e.split('_')[0] == 'INPUT', unique_elements)))
+	outputs_number = len(list(filter(lambda e: e.split('_')[0] == 'OUTPUT', unique_elements)))
+
+	macros_prefix = '__' + description['name'] + '__'
+	inputs_string = ', '.join([f'{macros_prefix}INPUT_{n}' for n in range(1, inputs_number+1)])
+	
+	for e in elements_inputs.keys():
+		result += "#define %s%s(%s) %s(%s)\n" % (
+			macros_prefix,
+			e,
+			inputs_string,
+			e.split('_')[0],
+			', '.join([(f'{macros_prefix}{i}({inputs_string})') if (i.split('_')[-2] != f'INPUT') else f'{macros_prefix}{i}' for i in elements_inputs[e].values()])
+		)
+	
+	result += '#define %s(%s) %s(%s)' % (
+		description['name'],
+		inputs_string,
+		', '.join([f'{macros_prefix}OUTPUT_{n}' for n in range(1, outputs_number+1)]),
+		inputs_string
+	)
+	
+	return result
+
 def compile(file_path):
 	result = '''#include <stdio.h>
 #include <stdlib.h>
@@ -14,41 +55,11 @@ def compile(file_path):
 	with open('example.json', 'r') as f:
 		program = json.load(f)
 	result += '\n\n'.join([defineFunction(description) for description in program])
-	return result
 
-def defineFunction(description):
-	result = ''
+	result += '''
 
-	unique_elements = {}
-	elements_inputs = {}
-	for w in description['wires']:
-		unique_elements[w['from']] = None
-		unique_elements[w['to']] = None
-		element = w['to']
-		if not (element in elements_inputs):
-			elements_inputs[element] = []
-		elements_inputs[element].append(w['from'])
+int main(void) {
+	printf("%d", xorxor(0, 1));
+}'''
 
-	inputs_number = len(list(filter(lambda e: e.split('_')[0] == 'INPUT', unique_elements)))
-	outputs_number = len(list(filter(lambda e: e.split('_')[0] == 'OUTPUT', unique_elements)))
-
-	macros_prefix = '__' + description['name'] + '__'
-	inputs_string = ', '.join([f'{macros_prefix}INPUT_{n}' for n in range(1, inputs_number+1)])
-	
-	for e in elements_inputs:
-		result += "#define %s%s(%s) %s(%s)\n" % (
-			macros_prefix,
-			e,
-			inputs_string,
-			e.split('_')[0],
-			', '.join([(f'{macros_prefix}{i}({inputs_string})') if (i.split('_')[-2] != f'{macros_prefix}INPUT') else i for i in elements_inputs[e]])
-		)
-	
-	result += '#define %s(%s) %s(%s)' % (
-		description['name'],
-		inputs_string,
-		', '.join([f'{macros_prefix}OUTPUT_{n}' for n in range(1, outputs_number+1)]),
-		inputs_string
-	)
-	
 	return result
