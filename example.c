51ee1196d30cd878cff278476b799e9b0750614f
mentalblood <neceporenkostepan@gmail.com>
2021-04-16 09:32:03 +0300

    fixed bugs with naming elements

--- example.c
+++ example.c
@@ -14,42 +14,26 @@
 
 #define NTH_1(...) FIRST(__VA_ARGS__)
 
-#define NTH_2(...) NTH_1(REST(__VA_ARGS__))
 
 
-#define __OR_3__OR_2(__OR_3__INPUT_1, __OR_3__INPUT_2, __OR_3__INPUT_3) OR(NTH_1(__OR_3__INPUT_1), NTH_1(__OR_3__INPUT_2))
-#define __OR_3__OR_1(__OR_3__INPUT_1, __OR_3__INPUT_2, __OR_3__INPUT_3) OR(NTH_1(__OR_3__OR_2(__OR_3__INPUT_1, __OR_3__INPUT_2, __OR_3__INPUT_3)), NTH_1(__OR_3__INPUT_3))
-#define __OR_3__OUTPUT_1(__OR_3__INPUT_1, __OR_3__INPUT_2, __OR_3__INPUT_3) OUTPUT(NTH_1(__OR_3__OR_1(__OR_3__INPUT_1, __OR_3__INPUT_2, __OR_3__INPUT_3)))
-#define OR_3(__OR_3__INPUT_1, __OR_3__INPUT_2, __OR_3__INPUT_3) __OR_3__OUTPUT_1(__OR_3__INPUT_1, __OR_3__INPUT_2, __OR_3__INPUT_3)
+#define __xor__NOT_1(__xor__INPUT_1, __xor__INPUT_2) NOT(NTH_1(__xor__INPUT_1))
+#define __xor__NOT_2(__xor__INPUT_1, __xor__INPUT_2) NOT(NTH_1(__xor__INPUT_2))
+#define __xor__OR_1(__xor__INPUT_1, __xor__INPUT_2) OR(NTH_1(__xor__INPUT_2), NTH_1(__xor__INPUT_1))
+#define __xor__AND_1(__xor__INPUT_1, __xor__INPUT_2) AND(NTH_1(__xor__OR_1(__xor__INPUT_1, __xor__INPUT_2)), NTH_1(__xor__OR_2(__xor__INPUT_1, __xor__INPUT_2)))
+#define __xor__OUTPUT_1(__xor__INPUT_1, __xor__INPUT_2) OUTPUT(NTH_1(__xor__AND_1(__xor__INPUT_1, __xor__INPUT_2)))
+#define __xor__OR_2(__xor__INPUT_1, __xor__INPUT_2) OR(NTH_1(__xor__NOT_2(__xor__INPUT_1, __xor__INPUT_2)), NTH_1(__xor__NOT_1(__xor__INPUT_1, __xor__INPUT_2)))
+#define xor(__xor__INPUT_1, __xor__INPUT_2) __xor__OUTPUT_1(__xor__INPUT_1, __xor__INPUT_2)
 
-#define __AND_3__AND_1(__AND_3__INPUT_1, __AND_3__INPUT_2, __AND_3__INPUT_3) AND(NTH_1(__AND_3__INPUT_1), NTH_1(__AND_3__INPUT_2))
-#define __AND_3__AND_2(__AND_3__INPUT_1, __AND_3__INPUT_2, __AND_3__INPUT_3) AND(NTH_1(__AND_3__AND_1(__AND_3__INPUT_1, __AND_3__INPUT_2, __AND_3__INPUT_3)), NTH_1(__AND_3__INPUT_3))
-#define __AND_3__OUTPUT_1(__AND_3__INPUT_1, __AND_3__INPUT_2, __AND_3__INPUT_3) OUTPUT(NTH_1(__AND_3__AND_2(__AND_3__INPUT_1, __AND_3__INPUT_2, __AND_3__INPUT_3)))
-#define AND_3(__AND_3__INPUT_1, __AND_3__INPUT_2, __AND_3__INPUT_3) __AND_3__OUTPUT_1(__AND_3__INPUT_1, __AND_3__INPUT_2, __AND_3__INPUT_3)
-
-#define __SUM__AND_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) AND(NTH_1(__SUM__INPUT_1), NTH_1(__SUM__INPUT_2))
-#define __SUM__AND_2(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) AND(NTH_1(__SUM__INPUT_1), NTH_1(__SUM__INPUT_3))
-#define __SUM__OR_3_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) OR_3(NTH_1(__SUM__INPUT_1), NTH_1(__SUM__INPUT_2), NTH_1(__SUM__INPUT_3))
-#define __SUM__AND_3_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) AND_3(NTH_1(__SUM__INPUT_1), NTH_1(__SUM__INPUT_2), NTH_1(__SUM__INPUT_3))
-#define __SUM__AND_3(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) AND(NTH_1(__SUM__INPUT_2), NTH_1(__SUM__INPUT_3))
-#define __SUM__OR_3_2(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) OR_3(NTH_1(__SUM__AND_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)), NTH_1(__SUM__AND_2(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)), NTH_1(__SUM__AND_3(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)))
-#define __SUM__OUTPUT_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) OUTPUT(NTH_1(__SUM__OR_3_2(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)))
-#define __SUM__NOT_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) NOT(NTH_1(__SUM__OR_3_2(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)))
-#define __SUM__AND_4(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) AND(NTH_1(__SUM__NOT_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)), NTH_1(__SUM__OR_3_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)))
-#define __SUM__OR_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) OR(NTH_1(__SUM__AND_3_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)), NTH_1(__SUM__AND_4(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)))
-#define __SUM__OUTPUT_2(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) OUTPUT(NTH_1(__SUM__OR_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)))
-#define SUM(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3) __SUM__OUTPUT_1(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3), __SUM__OUTPUT_2(__SUM__INPUT_1, __SUM__INPUT_2, __SUM__INPUT_3)
-
-#define test__all test_SUM
+#define test__all test_xor
 
 int main(void) {
 	printf("tests:\n");
-	int i;	printf("\tSUM:\n");
-	int tests_SUM__inputs[8][3] = {{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1}, {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}};
-	int tests_SUM__outputs[8][3] = {{0, 0}, {0, 1}, {0, 1}, {1, 0}, {0, 1}, {1, 0}, {1, 0}, {1, 1}};
-	for (i = 0; i < 8; i++) {
-		int output[2] = {SUM(tests_SUM__inputs[i][0], tests_SUM__inputs[i][1], tests_SUM__inputs[i][2])};
-		printf("\t\t[%d, %d, %d] => [%d, %d]: %s\n", tests_SUM__inputs[i][0], tests_SUM__inputs[i][1], tests_SUM__inputs[i][2], output[0], output[1], ((tests_SUM__outputs[i][0] == output[0]) && (tests_SUM__outputs[i][1] == output[1])) ? "passed" : "failed");
+	int i;	printf("\txor:\n");
+	int tests_xor__inputs[4][2] = {{0, 0}, {1, 1}, {1, 0}, {0, 1}};
+	int tests_xor__outputs[4][2] = {{0}, {0}, {1}, {1}};
+	for (i = 0; i < 4; i++) {
+		int output[1] = {xor(tests_xor__inputs[i][0], tests_xor__inputs[i][1])};
+		printf("\t\t[%d, %d] => [%d]: %s\n", tests_xor__inputs[i][0], tests_xor__inputs[i][1], output[0], ((tests_xor__outputs[i][0] == output[0])) ? "passed" : "failed");
 	}
 	return 0;
 }
